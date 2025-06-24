115dbadb611266a0470a0c5817a807a2d0c150f6
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-06-24 18:36:30 +0100

    Refactor sumAll function and update tests to remove skips and ensure proper functionality

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let result = 0;
+  if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      result += i;
+    }
+  } else {
+    result = "ERROR";
+  }
+  return result;
 };
 
 // Do not edit below this line
