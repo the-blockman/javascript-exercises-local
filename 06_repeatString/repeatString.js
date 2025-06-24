eb82a8a790db31a7eac4fb152dff2aee57135856
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-06-24 18:20:06 +0100

    Refactor repeatString function and update tests to remove skips and ensure proper functionality

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let i = 0;
+    let result = "";
 
+    while (i < num) {
+      result += string;
+      i++;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
